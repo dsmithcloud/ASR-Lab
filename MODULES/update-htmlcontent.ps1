81c48f7f0889a96a10d992644de8e3694bd3259f
David Smith <smithdavid@microsoft.com>
2024-09-27 16:46:35 -0500

    add file

---
+++ MODULES/update-htmlcontent.ps1
@@ -0,0 +1,10 @@
+$metadataUrl = "http://169.254.169.254/metadata/instance/compute?api-version=2021-02-01"
+$headers = @{ "Metadata" = "true" }
+
+$serverName=$response.name
+$location=$response.location
+
+$IndexFile=get-content -path c:\inetpub\wwwroot\index.html
+$UpdatedFile = $IndexFile -replace "ServerName",$serverName -replace "Location",$location
+$UpdatedFile
+Set-content -path c:\inetpub\wwwroot\index.html -Value $UpdatedFile
